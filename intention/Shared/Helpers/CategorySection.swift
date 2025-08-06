795ff7cc4ef6f92f0327c7cf659aebc351e71024
bbb4tryon <b.t.tryon@gmail.com>
2025-08-05 18:26:58 -0600

    refactor: CategorySection without onDrop

--- intention/Shared/Helpers/CategorySection.swift
+++ intention/Shared/Helpers/CategorySection.swift
@@ -6,17 +6,13 @@
 //
 
 import SwiftUI
-import UniformTypeIdentifiers
 
 struct CategorySection: View {
     @Binding var categoryItem: CategoriesModel
     let palette: ScreenStylePalette
     let fontTheme: AppFontTheme
     @Binding var newTextTiles: [UUID: String]
-    @Binding var dropTarget: Bool
     let saveHistory: () -> Void
-    let tileDropHandler: TileDropHandler
-    let moveTile: (TileM, UUID, UUID) async -> Void
 
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
@@ -31,26 +27,12 @@ struct CategorySection: View {
                 categoryItem: $categoryItem,
                 palette: palette,
                 fontTheme: fontTheme,
-                saveHistory: saveHistory,
-                tileDropHandler: tileDropHandler,
-                moveTile: moveTile
+                saveHistory: saveHistory
             )
-            // CAN REMOVE IF UIKIT HANDLES MOVING TILES
-//            .onDrop(of: [UTType.draggableTile.identifier], isTargeted: $dropTarget) { providers in
-//                //FIXME: use the extracted helper????
-//                Task {
-//                    if let dragged = await tileDropHandler.handleDrop(providers: providers),
-//                       dragged.fromCategoryID != categoryItem.id {
-//                        await moveTile(dragged.tile, dragged.fromCategoryID, categoryItem.id)
-//                    }
-//                }
-//                return true
-//            }
-            .background(dropTarget ? palette.accent.opacity(0.1) : Color.clear)
-            .animation(.easeInOut, value: dropTarget)
         }
         .padding(.horizontal)
         .padding(.bottom, 12)
     }
 }
+
 
